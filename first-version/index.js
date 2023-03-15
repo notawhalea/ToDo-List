1345f62253e8c1dcdac6c0069ff80cccd78bb401
Nikita <nickpelmog2003@gmail.com>
2023-03-16 00:12:41 +0300

    feat: add databases and models for it

--- first-version/index.js
+++ first-version/index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const sequelize = require('./utils/database')
+const toDoRoutes = require('./routes/todo')
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.json())
+app.use('/api/todo', toDoRoutes)
 app.use( (req, res, next) => {
     res.sendFile('/index.html')
 })
+async function start() {
+    try {
+        await sequelize.sync()
+        app.listen(PORT)
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
 
-app.listen(PORT)
+start()
+
