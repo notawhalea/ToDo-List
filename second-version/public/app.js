1abca48d0cc168c208f9581256979539c713f6d9
Nikita <nickpelmog2003@gmail.com>
2023-03-17 23:31:10 +0300

    feat: add methods for graphql

--- second-version/public/app.js
+++ second-version/public/app.js
@@ -10,14 +10,25 @@ new Vue({
         }
     },
     created() {
-        fetch('/api/todo', {
-            method: 'get'
+        const query = `
+            query {
+                getTodos {
+                    id title done createdAt updatedAt
+                }
+            }
+        `
+        fetch('/graphql', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify( { query })
         })
             .then(res => res.json())
-            .then(todos => {
-                this.todos = todos
+            .then(response => {
+                this.todos = response.data.getTodos
             })
-            .catch(e => console.log(e))
     },
     methods: {
         addTodo() {
@@ -25,21 +36,42 @@ new Vue({
             if (!title) {
                 return
             }
-            fetch('/api/todo', {
+            const query = `
+        mutation {
+          createTodo(todo: {title: "${title}"}) {
+            id title done createdAt updatedAt
+          }
+        }
+      `
+            fetch('/graphql', {
                 method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({title})
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({query})
             })
                 .then(res => res.json())
-                .then(({todo}) => {
+                .then(response => {
+                    const todo = response.data.createTodo
                     this.todos.push(todo)
                     this.todoTitle = ''
                 })
-                .catch((e) => console.log(e))
+                .catch(e => console.log(e))
         },
         removeTodo(id) {
-            fetch('/api/todo/' + id, {
-                method: 'delete'
+            const query = `
+        mutation {
+          deleteTodo(id: "${id}")
+        }
+      `
+            fetch('/graphql', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({ query })
             })
                 .then(() => {
                     this.todos = this.todos.filter(t => t.id !== id)
@@ -47,15 +79,25 @@ new Vue({
                 .catch(e => console.log(e))
         },
         completeTodo(id) {
-            fetch('/api/todo/' + id, {
-                method: 'put',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({done: true})
+            const query = `
+            mutation {
+                completeTodo(id: "${id}") {
+                    updatedAt
+                }
+            }
+            `
+            fetch('/graphql', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({ query })
             })
                 .then(res => res.json())
-                .then(({todo}) => {
-                    const idx = this.todos.findIndex(t => t.id === todo.id)
-                    this.todos[idx].updatedAt = todo.updatedAt
+                .then(response => {
+                    const idx = this.todos.findIndex(t => t.id === id)
+                    this.todos[idx].updatedAt = response.data.completeTodo.updatedAt
                 })
                 .catch(e => console.log(e))
         }
@@ -75,7 +117,7 @@ new Vue({
                 options.minute = '2-digit'
                 options.second = '2-digit'
             }
-            return new Intl.DateTimeFormat('en-EN', options).format(new Date(value))
+            return new Intl.DateTimeFormat('en-EN', options).format(new Date(+value))
         }
     }
 })
